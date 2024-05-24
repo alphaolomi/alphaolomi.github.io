bdf4415688fcbce2478dae7a67a74ede38d16ea2
Alpha Olomi <alphaolomi@gmail.com>
2024-05-24 11:24:15 +0300

    full revamp

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # alphaolomi.github.io
+
+## Tech
+
+- MDX and Markdown support
+- Optimized for SEO (sitemap, robots, JSON-LD schema)
+- RSS Feed
+- Dynamic OG images
+- Syntax highlighting
+- Tailwind v4
+- Vercel Speed Insights / Web Analytics
+- Geist font
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,42 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './global.css'
+import type { Metadata } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import { Navbar } from './components/nav'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import Footer from './components/footer'
+import { baseUrl } from './sitemap'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Alpha Olomi | Software Engineer',
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: 'Alpha Olomi | Software Engineer',
+    template: '%s | Alpha Olomi',
+  },
   description: 'Software Engineer based in Dar es Salaam, Tanzania',
+  openGraph: {
+    title: 'Alpha Olomi | Software Engineer',
+    description: 'Software Engineer based in Dar es Salaam, Tanzania',
+    url: baseUrl,
+    siteName: 'Alpha Olomi',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
+
+const cx = (...classes) => classes.filter(Boolean).join(' ')
 
 export default function RootLayout({
   children,
@@ -14,8 +44,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-black',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
+      <body className="max-w-xl mx-4 mt-8 antialiased lg:mx-auto">
+        <main className="flex flex-col flex-auto min-w-0 px-2 mt-6 md:px-0">
+          <Navbar />
+          {children}
+          <Footer />
+          <Analytics />
+          <SpeedInsights />
+        </main>
+      </body>
     </html>
   )
 }
